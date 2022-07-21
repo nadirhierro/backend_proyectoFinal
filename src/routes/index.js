0057c45537183ad1676f4f0a62c03fd455833355
Nadir Hierro <nadir.hierro@gmail.com>
2022-07-21 15:12:16 -0300

    Router home y auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import routerApi from "./API/index.js";
+import routerHome from "./routerHome/index.js";
 
 let { Router } = express;
 
 let router = new Router();
 
 router.use("/api", routerApi);
+router.use("/", routerHome);
 
 export default router;
---
+++ src/routes/routerHome/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import AuthController from "../../components/controllers/auth/index.js";
+
+const { Router } = express;
+
+let auth = new AuthController();
+
+let routerHome = new Router();
+
+routerHome.get("/login", auth.goLogin);
+
+routerHome.get("/signup", auth.goSignUp);
+
+routerHome.get("/", auth.isAuth, auth.goIndex);
+
+routerHome.post("/login", auth.makeLogin);
+
+routerHome.get("/logout", auth.makeLogout);
+
+routerHome.post("/signup", auth.uploadAvatar, auth.makeSignup);
+
+routerHome.get("/home", auth.isAuth, auth.goHome);
+
+routerHome.get("/faillogin", auth.failLogin);
+
+routerHome.get("/failsignup", auth.failSignup);
+
+export default routerHome;
