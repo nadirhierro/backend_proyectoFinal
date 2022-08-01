c10b794a34bf77fcbd07f87a9d3301a861793fcd
nadirhierro <84636489+nadirhierro@users.noreply.github.com>
2022-08-01 15:08:43 -0300

    corrección orders

--- src/components/containers/daos/ordersContainers/memoryDaoOrders.js
+++ src/components/containers/daos/ordersContainers/memoryDaoOrders.js
@@ -14,4 +14,15 @@ export default class memoryDaoOrders extends memoryContainer {
     }
     return instance;
   }
+  
+  async saveOrder(order){
+    try {
+      let finalOrder = {...order, state: "generated"};
+      let saved = await this.save(finalOrder);
+      return saved;
+    } catch (err) {
+      this.logger.logDatabaseError(err);
+      throw new Error("Database Error");
+    }
+  }
 }
